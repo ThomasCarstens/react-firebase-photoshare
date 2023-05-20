069706344ccf2b6f8227281fb9789760ce2165bb
Thomas Carstens <thomaxarstens@gmail.com>
2023-05-20 17:01:56 +0200

    auth,login,logout

---
+++ firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth, initializeAuth } from "firebase/auth";
+import { getReactNativePersistence } from '@react-native-firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPSdB4UCecFGGoAwggKuM0aUjKuLXTgis",
+  authDomain: "fir-auth-31f82.firebaseapp.com",
+  projectId: "fir-auth-31f82",
+  storageBucket: "fir-auth-31f82.appspot.com",
+  messagingSenderId: "759808068250",
+  appId: "1:759808068250:web:31847fbc6bc1455853ed23"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
